a8acf97569396fad5e2aec6a2a692325069095f8
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-04-05 19:07:31 +0700

    added empty actions that I want to do

---
+++ src/main/java/com/robohorse/robopojogenerator/actions/CacheLayerPOJOAction.kt
@@ -0,0 +1,11 @@
+package com.robohorse.robopojogenerator.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class CacheLayerPOJOAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent?) {
+
+    }
+
+}
---
+++ src/main/java/com/robohorse/robopojogenerator/actions/DomainLayerPOJOAction.kt
@@ -0,0 +1,18 @@
+package com.robohorse.robopojogenerator.actions
+
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class DomainLayerPOJOAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent?) {
+        val notification = NotificationGroup("Not Implemented", NotificationDisplayType.STICKY_BALLOON, true)
+        notification.createNotification("Not Implemented",
+                "Will be implemented soon",
+                NotificationType.INFORMATION,
+                null
+        ).notify(e?.project)
+    }
+}
