47ab77cf1bf8f67dd39f22534c090bf07cc27574
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-04-07 14:22:16 +0700

    added domain & cache action

--- src/main/java/com/robohorse/robopojogenerator/actions/CacheLayerPOJOAction.kt
+++ src/main/java/com/robohorse/robopojogenerator/actions/CacheLayerPOJOAction.kt
@@ -2,10 +2,39 @@ package com.robohorse.robopojogenerator.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.robohorse.robopojogenerator.controllers.POJOGeneratorActionController
+import com.robohorse.robopojogenerator.generator.consts.annotations.AnnotationEnum
+import com.robohorse.robopojogenerator.generator.consts.templates.ArrayItemsTemplate
+import com.robohorse.robopojogenerator.generator.consts.templates.ClassTemplate
+import com.robohorse.robopojogenerator.injections.Injector
+import com.robohorse.robopojogenerator.models.GenerationModel
+import javax.inject.Inject
 
 class CacheLayerPOJOAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent?) {
 
+    @Inject
+    lateinit var generatePOJOActionController: POJOGeneratorActionController
+
+    init {
+        Injector.initModules()
+        Injector.getAppComponent().inject(this)
     }
 
+    override fun actionPerformed(e: AnActionEvent) {
+        val generationModel = GenerationModel.Builder()
+                .useKotlin(true)
+                .setAnnotationItem(AnnotationEnum.NONE)
+                .setSettersAvailable(false)
+                .setGettersAvailable(false)
+                .setToStringAvailable(false)
+                .setRewriteClasses(true)
+                .setPrefix("Cached")
+                .setSuffix("")
+                .setFieldDTOFormat(ClassTemplate.NON_NULL_FIELD_KOTLIN_DTO)
+                .setListFormat(ArrayItemsTemplate.NON_NULL_LIST_OF_ITEM)
+                .setDialogTitle("Cache POJO Generator")
+                .build()
+
+        generatePOJOActionController.onActionHandled(e, generationModel)
+    }
 }
