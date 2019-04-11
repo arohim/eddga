fb0e7f340076886bcfb0ba540ce961461b093817
arohim <a.rohim.sama@gmail.com>
2019-04-11 22:25:41 +0700

    upgrade mockito

--- src/test/java/com/robohorse/robopojogenerator/generator/MapperCreatorTest.kt
+++ src/test/java/com/robohorse/robopojogenerator/generator/MapperCreatorTest.kt
@@ -1,24 +1,44 @@
 package com.robohorse.robopojogenerator.generator
 
+import com.robohorse.robopojogenerator.delegates.FileTemplateWriterDelegate
 import com.robohorse.robopojogenerator.generator.common.ClassField
+import com.robohorse.robopojogenerator.generator.common.ClassItem
 import com.robohorse.robopojogenerator.generator.common.MapperCreator
 import com.robohorse.robopojogenerator.generator.consts.ClassEnum
+import com.robohorse.robopojogenerator.generator.processing.ClassProcessor
+import com.robohorse.robopojogenerator.generator.utils.ClassGenerateHelper
+import com.robohorse.robopojogenerator.models.GenerationModel
+import com.robohorse.robopojogenerator.models.MapperGeneratorModel
+import com.robohorse.robopojogenerator.models.ProjectModel
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 import java.util.LinkedHashMap
+import javax.inject.Inject
 
+@RunWith(MockitoJUnitRunner::class)
 class MapperCreatorTest {
+
+    @Mock
+    lateinit var classGenerateHelper: ClassGenerateHelper
+
+    @Mock
+    lateinit var processor: ClassProcessor
+
+    @Mock
+    lateinit var roboPOJOGenerator: RoboPOJOGenerator
+
+    @Mock
+    lateinit var fileTemplateWriterDelegate: FileTemplateWriterDelegate
 
     @InjectMocks
     lateinit var mapperCreator: MapperCreator
-
-    @Before
-    fun setUp() {
-        MockitoAnnotations.initMocks(this)
-    }
 
     @Test
     fun `Generate mapping field string`() {
@@ -36,5 +56,21 @@ class MapperCreatorTest {
                 "propB = type.propB,\n" +
                 "propC = type.propC"
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Generate file`() {
+        // GIVEN
+        val generationModel = GenerationModel.Builder().build()
+        val projectModel = ProjectModel.Builder().build()
+        val mapperGeneratorModel = MapperGeneratorModel(fileNameSuffix = "", templateName = "")
+        val classItems = HashSet<ClassItem>()
+        `when`(roboPOJOGenerator.generate(generationModel)).thenReturn(classItems)
+
+        // WHEN
+        mapperCreator.generateFiles(generationModel, projectModel, mapperGeneratorModel)
+
+        // THEN
+
     }
 }
