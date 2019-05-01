817833013a7935a223d826a37f04d934b852e487
arohim <a.rohim.sama@gmail.com>
2019-05-01 23:58:10 +0700

    auto generate default value if null able fields

--- src/main/java/com/robohorse/robopojogenerator/generator/common/MapperCreator.kt
+++ src/main/java/com/robohorse/robopojogenerator/generator/common/MapperCreator.kt
@@ -3,6 +3,7 @@ package com.robohorse.robopojogenerator.generator.common
 import com.robohorse.robopojogenerator.delegates.FileTemplateWriterDelegate
 import com.robohorse.robopojogenerator.errors.RoboPluginException
 import com.robohorse.robopojogenerator.generator.RoboPOJOGenerator
+import com.robohorse.robopojogenerator.generator.consts.ClassEnum
 import com.robohorse.robopojogenerator.generator.utils.ClassGenerateHelper
 import com.robohorse.robopojogenerator.models.GenerationModel
 import com.robohorse.robopojogenerator.models.MapperGeneratorModel
@@ -29,8 +30,10 @@ open class MapperCreator @Inject constructor() {
             val fileTemplateManager = fileTemplateWriterDelegate.getInstance(projectModel.project)
             val templateProperties = fileTemplateManager.defaultProperties
             templateProperties["CLASS_NAME"] = classItem.className
-            templateProperties["MAP_TO_ENTITIES"] = generateMappingFieldString(classItem.classFields, mapperGeneratorModel.fileNameSuffix, mapperGeneratorModel.mapToMethodName)
-            templateProperties["MAP_FROM_ENTITIES"] = generateMappingFieldString(classItem.classFields, mapperGeneratorModel.fileNameSuffix, mapperGeneratorModel.mapFromMethodName)
+            templateProperties["MAP_TO_ENTITIES"] = generateMappingFieldString(classItem.classFields,
+                    mapperGeneratorModel.fileNameSuffix, mapperGeneratorModel.mapToMethodName, false)
+            templateProperties["MAP_FROM_ENTITIES"] = generateMappingFieldString(classItem.classFields,
+                    mapperGeneratorModel.fileNameSuffix, mapperGeneratorModel.mapFromMethodName, false)
             templateProperties["INJECTORS"] = generateInjectors(classItem.classFields, mapperGeneratorModel.fileNameSuffix)
             val fileName = classItem.className + mapperGeneratorModel.fileNameSuffix
             fileTemplateWriterDelegate.writeTemplate(
@@ -53,27 +56,59 @@ open class MapperCreator @Inject constructor() {
         return injectors.joinToString(",\n")
     }
 
-    fun generateMappingFieldString(classFields: MutableMap<String, ClassField>, suffix: String, mapperMethod: String): String {
+    fun generateMappingFieldString(classFields: MutableMap<String, ClassField>, suffix: String, mapperMethod: String, isNullable: Boolean): String {
+        val nullSafety = if (isNullable) "?" else ""
+
         val asserts = mutableListOf<String>()
         classFields.forEach {
             val fieldName = generateHelper.formatClassField(it.key)
+            val defaultValue = if (isNullable) getDefaultValue(it.value) else ""
 
             val assert = when {
                 isClassField(it.value) -> {
                     val mapperName = generateHelper.formatClassField("${it.key}$suffix")
-                    "$fieldName = $mapperName.$mapperMethod(type.$fieldName)"
+                    "$fieldName = $mapperName.$mapperMethod(type$nullSafety.$fieldName)$defaultValue"
                 }
                 it.value.isListField -> {
                     val mapperName = generateHelper.formatClassField("${it.key}$suffix")
-                    "$fieldName = type.$fieldName.map { $mapperName.$mapperMethod(it) }"
+                    "$fieldName = type.$fieldName$nullSafety.map { $mapperName.$mapperMethod(it) }$defaultValue"
                 }
                 else -> {
-                    "$fieldName = type.$fieldName"
+                    "$fieldName = type$nullSafety.$fieldName$defaultValue"
                 }
             }
             asserts.add(assert)
         }
         return asserts.joinToString(",\n")
+    }
+
+    private fun getDefaultValue(classField: ClassField): String {
+        return when (classField.classEnum) {
+            ClassEnum.STRING -> {
+                " ?: defaultString"
+            }
+            ClassEnum.INTEGER -> {
+                " ?: defaultInt"
+            }
+            ClassEnum.BOOLEAN -> {
+                " ?: defaultBoolean"
+            }
+            ClassEnum.LONG -> {
+                " ?: defaultLong"
+            }
+            ClassEnum.FLOAT -> {
+                " ?: defaultFloat"
+            }
+            ClassEnum.DOUBLE -> {
+                " ?: defaultDouble"
+            }
+            else -> {
+                if (classField.isListField) {
+                    return " ?: listOf()"
+                }
+                ""
+            }
+        }
     }
 
     private fun isClassField(value: ClassField): Boolean {
